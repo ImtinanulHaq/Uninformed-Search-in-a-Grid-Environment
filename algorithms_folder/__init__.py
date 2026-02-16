ebf080486aac4e09d33c1be799b118eb77c3cd75
Student Developer <student@university.edu>
2026-02-16 22:51:03 +0500

    feat: add modular algorithm implementations in separate files for better understanding

---
+++ algorithms_folder/__init__.py
@@ -0,0 +1,8 @@
+from .bfs import BFS
+from .dfs import DFS
+from .ucs import UCS
+from .dls import DLS
+from .iddfs import IDDFS
+from .bidirectional import BidirectionalSearch
+
+__all__ = ['BFS', 'DFS', 'UCS', 'DLS', 'IDDFS', 'BidirectionalSearch']
